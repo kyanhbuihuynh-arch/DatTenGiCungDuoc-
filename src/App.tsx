f747060d4aac7677911183eb25d004973a8b8e1f
Ky Anh <kyanhbuihuynh@gmail.com>
2026-04-14 13:00:08 +0700

    new

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ export default function App() {
                 New: Version 2.0 is live
               </span>
               <h1 className="text-5xl lg:text-7xl font-bold tracking-tight text-slate-900 mb-8 leading-[1.1]">
-                Connect your team <br />
+                Đây là web của Kỳ Anh!!! <br />
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-violet-600">
                   without the friction.
                 </span>
